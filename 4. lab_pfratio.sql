528e4daad2a32e6ad375bd5ddf75ccd9954b5656
jia-tang <54199432+jia-tang@users.noreply.github.com>
2020-05-21 10:44:39 +0800

    Update 4. lab_pfratio.sql

--- 4. lab_pfratio.sql
+++ 4. lab_pfratio.sql
@@ -1,15 +1,28 @@
-drop table if exists `ync-capstones.Jia.lab_pfratio`;
-create table `ync-capstones.Jia.lab_pfratio` as
+--drop table if exists `ync-capstones.Jia.lab_pfratio`;
+--create table `ync-capstones.Jia.lab_pfratio` as
 with v1 as(
 select patientunitstayid, chartoffset
-,fio2, pao2
-, pao2/fio2 as pfratio
-, round(chartoffset/60) as hr1, round(chartoffset/180) as hr3, round(chartoffset/300) as hr5, round(chartoffset/420) as hr7
-, round(chartoffset/540) as hr9, round(chartoffset/720) h12, round(chartoffset/1440) h24
+,fio2
+from `ync-capstones.Jia.lab_keys_raw`
+where fio2 is not null
+order by patientunitstayid,chartoffset),
+
+v2 as (
+select patientunitstayid, chartoffset, pao2
 from `ync-capstones.Jia.lab_keys_raw`
 where pao2 is not null
-and fio2 is not null
-order by patientunitstayid,chartoffset)
+order by patientunitstayid,chartoffset),
+
+v3 as (
+select v1.patientunitstayid, v1.chartoffset as pf_offset, v2.pao2, v1.fio2
+from v1
+inner join v2 
+on v1.patientunitstayid=v2.patientunitstayid
+and v1.chartoffset = v2.chartoffset), -- didn't use 1h interval, as doesn't increase much data, and may result in repetitive data
+
+v4 as (
+select *, pao2/fio2 as pfratio
+from v3)
 
 SELECT *, 
     CASE 
@@ -17,6 +30,6 @@ SELECT *,
         WHEN pfratio between 100 and 200 THEN "moderate"
         WHEN pfratio between 200 and 300 THEN "mild"
     END AS groupx
-FROM v1
+FROM v4
 where pfratio<=300
-
+order by patientunitstayid, pf_offset
