fa69dd576bba67e28462fd047301b1d502abd9f8
jia-tang <54199432+jia-tang@users.noreply.github.com>
2020-05-21 10:42:22 +0800

    Update 2. lungcompliance without null.sql

--- 2. lungcompliance without null.sql
+++ 2. lungcompliance without null.sql
@@ -1,14 +1,12 @@
-drop table if exists `ync-capstones.Jia.lungcompliance`;
+drop view if exists `ync-capstones.Jia.lungcompliance`;
 
-create table `ync-capstones.Jia.lungcompliance` as 
+create view `ync-capstones.Jia.lungcompliance` as 
 
 with v1 as(
 select patientunitstayid, chartoffset
 , cast(Tidal_Volume as numeric) as tidal_volume
 ,  cast(Plateau_Pressure as numeric) as plateau_pressure
 ,  cast(peep as numeric) as peep
-, round(chartoffset/60) as hr1, round(chartoffset/180) as hr3, round(chartoffset/300) as hr5, round(chartoffset/420) as hr7
-, round(chartoffset/540) as hr9, round(chartoffset/720) h12, round(chartoffset/1440) h24
 from `ync-capstones.Jia.lungcompliance_raw`)
 
 
