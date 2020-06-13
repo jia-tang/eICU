ebeba59d61a9d6f5cd5ae8a58c9ae5cd7283b87d
jia-tang <54199432+jia-tang@users.noreply.github.com>
2020-06-13 13:18:52 +0800

    Update 4.day12.sql

--- 4.day12.sql
+++ 4.day12.sql
@@ -1,21 +1,15 @@
 with v1 as (
 select *
-,    CASE 
-        WHEN lung_compliance <= 26.47 THEN "S4"
-        WHEN lung_compliance between 26.47 and 33.33 THEN "S3"
-        WHEN lung_compliance between 33.33 and 44 THEN "S2"
-        WHEN lung_compliance >= 44 THEN "S1"
-    END AS compliance_level
 ,ROW_NUMBER() OVER (partition by patientunitstayid order by ABS(chartoffset-0) asc) as ranked_0hour
 from `ync-capstones.Jia.lungcompliance`), 
 
-d1 as (
+d1 as ( -- Day 1 compliance: the first compliance on Day 1
 select *
 from v1
 where abs(chartoffset-0)<24*60
 and ranked_0hour=1),
 
-d2 as (
+d2 as ( -- Day 2 compliance: the worst compliance on Day 2
 select v1.patientunitstayid,lung_compliance from v1
 inner join 
 (select v1.patientunitstayid, min(lung_compliance) as min_compliance
@@ -24,9 +18,18 @@ on v1.patientunitstayid=t.patientunitstayid and v1.lung_compliance=t.min_complia
 where abs(chartoffset-0) between 24*60 and 47*60)
 
 select min.patientunitstayid, d1.lung_compliance as day1_compliance
-, d2.lung_compliance as day2_compliance from `ync-capstones.Jia.patient_min_pfratio` min
+, d2.lung_compliance as day2_compliance from `ync-capstones.Jia.combined_minpf_compliance` min
 inner join d1 
 on d1.patientunitstayid=min.patientunitstayid
 inner join d2
 on d2.patientunitstayid=min.patientunitstayid
 order by patientunitstayid,chartoffset
+
+
+--102
+
+-- 1926 data with day 1 from lung_compliance
+-- 319 data with day 2 from lung_compliance
+-- combine with pfratio on day1 patient: 102 data
+
+
