a6ddfbe905d86cc29345445a2e6e4c6a08d42e59
jia-tang <54199432+jia-tang@users.noreply.github.com>
2020-08-03 11:32:14 +0800

    Update 5.day12.sql

--- 5.day12.sql
+++ 5.day12.sql
@@ -1,12 +1,17 @@
+--1511 patients
+
 with v1 as (
-select *
-,ROW_NUMBER() OVER (partition by patientunitstayid order by ABS(chartoffset-0) asc) as ranked_0hour
-from `ync-capstones.Jia.lungcompliance`), 
+select compliance.*
+,ROW_NUMBER() OVER (partition by compliance.patientunitstayid order by ABS(TV_offset-0) asc) as ranked_0hour
+from `ync-capstones.Jia.lungcompliance` compliance
+inner join `ync-capstones.Jia.combined_firstpf_compliance` pf
+on compliance.patientunitstayid=pf.patientunitstayid
+), 
 
 d1 as ( -- Day 1 compliance: the first compliance on Day 1
 select *
 from v1
-where abs(chartoffset-0)<24*60
+where abs(TV_offset-0)<24*60
 and ranked_0hour=1),
 
 d2 as ( -- Day 2 compliance: the worst compliance on Day 2
@@ -14,7 +19,7 @@ select v1.patientunitstayid,lung_compliance from v1
 inner join 
 (select v1.patientunitstayid, min(lung_compliance) as min_compliance
 from v1 
-where chartoffset between 24*60 and 47*60
+where TV_offset between 24*60 and 47*60
 group by patientunitstayid) t
 on v1.patientunitstayid=t.patientunitstayid and v1.lung_compliance=t.min_compliance
 group by patientunitstayid,lung_compliance
@@ -28,17 +33,16 @@ inner join d1
 on d1.patientunitstayid=first.patientunitstayid
 inner join d2
 on d2.patientunitstayid=first.patientunitstayid
-order by patientunitstayid,chartoffset)
+order by patientunitstayid,TV_offset)
 
 select * ,CASE 
-        WHEN day1 <= 35 THEN "L"
-        WHEN day1 >= 35 THEN "H"
+        WHEN day1 <= 34 THEN "L"
+        When day1 >= 34 THEN "H"
     END AS day1_compliance
     , CASE 
-        WHEN day2 <= 35 THEN "L"
-        WHEN day2 >= 35 THEN "H"
+        WHEN day2 <= 34 THEN "L"
+        When day2 >= 34 THEN "H"
     END AS day2_compliance
 from final
--- 2002 data
 
 
